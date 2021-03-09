6283839ea92ada889d32ec3491c122ecb6ca9a76
Faysal Ahmed <77103788+faysal-ahmed-akash@users.noreply.github.com>
2021-03-09 21:30:15 +0600

    Update bubble-sort.cpp

--- bubble-sort.cpp
+++ bubble-sort.cpp
@@ -1,4 +1,5 @@
-//Best case: O(n), when the array is already sorted
+//Best case: O(n), when the array is already sorted, this is for optimized bubble sort (when we use flag to keep track of swapping)
+//Best case: O(n^2), for raw bubble sort
 //Worst case: O(n^2), when the array is reverse sorted
 //Average case: O(n^2)
 //Space: O(1), as it's an in place algorithm
@@ -9,16 +10,16 @@ using namespace std;
 
 int main(){
     int arr[] = {64, 34, 25, 12, 22, 11, 90};  
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n = sizeof(arr) / sizeof(arr[0]);
     
     bool flag;
     
     for(int i = 0; i < n-1; i++){
         flag = false;
         for(int j = 0; j < n-i-1; j++){
-		//at every pass the largest element placed correctly at the last.
-        //so we don't to loop for that element. That's why we subtract i
-            if(arr[j]>arr[j+1]){
+	//at every pass the largest element placed correctly at the last.
+        //so we don't have to loop for that element. That's why we subtract i
+            if(arr[j] > arr[j+1]){
                 swap(arr[j], arr[j+1]);
                 flag = true;
             }
